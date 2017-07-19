d6c2f2838c33b084e790ecaa495bb121e83c8edd
pb <419995258@qq.com>
2017-07-19 15:23:54 +0800

    优化代码

--- javaTestList/src/testList/test.java
+++ javaTestList/src/testList/test.java
@@ -2,77 +2,82 @@ package testList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
-
+import java.util.Set;
 
 public class test {
 
-	public static void main(String[] args){
-		String[] arr = {"1","2"};
+	public static void main(String[] args) {
+		String[] arr = { "1", "2" };
 		List<String> list = new ArrayList<String>(Arrays.asList(arr));
 		System.out.println(list);
 		String str2 = new String("a");
-		
 		StringBuffer sb = new StringBuffer("b");
 		sb.append("c");
-		
+
 		Integer a = new Integer(100);
 		Integer b = new Integer(100);
 		System.out.println(a == b);
 		String str = new String("100");
 		System.out.println(a.equals(Integer.valueOf(str)));
-		
-		
-		
-		long startTime= 0;
+
+		long startTime = 0;
 		long endTime = 0;
 		System.out.println("程序时间运行计算string");
 		startTime = System.currentTimeMillis();
 		String test1 = "a";
-		for(int i=0;i<10000;i++){
+		for (int i = 0; i < 10000; i++) {
 			test1 = test1 + i;
 		}
 		endTime = System.currentTimeMillis();
-		System.out.println("time:" + (endTime-startTime));
-		
+		System.out.println("time:" + (endTime - startTime));
+
 		System.out.println("程序时间运行计算stringBuffer");
 		startTime = System.currentTimeMillis();
 		StringBuffer test2 = new StringBuffer("a");
-		for(int i=0;i<10000;i++){
+		for (int i = 0; i < 10000; i++) {
 			test2 = test2.append(i);
 		}
 		endTime = System.currentTimeMillis();
-		System.out.println("time:" + (endTime-startTime));
-		
-		
+		System.out.println("time:" + (endTime - startTime));
+
 		System.out.println("程序时间运行计算stringBudder");
 		startTime = System.currentTimeMillis();
 		StringBuilder test3 = new StringBuilder("a");
-		for(int i=0;i<10000;i++){
+		for (int i = 0; i < 10000; i++) {
 			test3 = test3.append(i);
 		}
 		endTime = System.currentTimeMillis();
-		System.out.println("time:" + (endTime-startTime));
-		
-		
-		long startTime2= 0;
+		System.out.println("time:" + (endTime - startTime));
+
+		long startTime2 = 0;
 		long endTime2 = 0;
 		System.out.println("程序时间运行计算namo string");
 		startTime = System.nanoTime();
 		String testnamo1 = "a" + "b";
 		endTime = System.nanoTime();
-		System.out.println("time:" + (endTime2-startTime2));
-		
+		System.out.println("time:" + (endTime2 - startTime2));
+
 		System.out.println("程序时间运行计算namo stringBuffer");
 		startTime = System.currentTimeMillis();
 		StringBuffer testnamo2 = new StringBuffer("a").append("b");
 		endTime = System.currentTimeMillis();
-		System.out.println("time:" + (endTime2-startTime2));
-		
-		List list3 = null;		
-		
+		System.out.println("time:" + (endTime2 - startTime2));
+
+		List list3 = null;
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		map.put("k1", "v1");
+		map.put("k2", "v2");
+		System.out.println(map.containsValue("v1"));
+		Set<Object> set = map.keySet();
+		Collection<Object> list4 =  map.values();
+		System.out.println(set);
 	}
-	
+
 }
--- javaTestList/src/testThread/ThreadPool/CachedThreadPool.java
+++ javaTestList/src/testThread/ThreadPool/CachedThreadPool.java
@@ -15,12 +15,12 @@ public class CachedThreadPool {
     public static  void main(String[] args){
         ExecutorService threadPool = Executors.newCachedThreadPool();
 
-        for(int i=1; i<=10; i++){
+        for(int i=1; i<=100; i++){
             final int taskId = i;
             threadPool.execute(new Runnable(){
 
                 public void run() {
-                    for(int i=1; i<=10; i++){
+                    for(int i=1; i<=100; i++){
                         System.out.println(Thread.currentThread().getName() + " is looping of " + i + " the task is " + taskId);
                         try {
                             Thread.sleep(1000);
