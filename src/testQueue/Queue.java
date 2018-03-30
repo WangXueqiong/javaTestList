403160b3f1638f913081559b26cd11beb4042d7b
pb <419995258@qq.com>
2018-03-30 14:55:28 +0800

    添加一些队列的备注

--- src/testQueue/Queue.java
+++ src/testQueue/Queue.java
@@ -13,16 +13,19 @@ public class Queue {
 		System.out.println(queue.peek());
 		Boolean status = false;
 		for(int i=1;i<11;i++){
+			//在队列queue中塞入数值
 			queue.offer(i);
 			System.out.println("queue的size" + queue.size());
 		}
 		System.out.println(queue);
 		queue.poll();
 		System.out.println("poll()方法");
+		//poll         移除并返问队列头部的元素    如果队列为空，则返回null
 		System.out.println(queue);
 		System.out.println("queue的size" + queue.size());
 		Integer i = queue.peek();
 		System.out.println("peek()方法");
-		System.out.println(i);
+		//peek       返回队列头部的元素             如果队列为空，则返回null
+        System.out.println(i);
 	}
 }
